9a16de88439e2754e41340527cc0978b2d8bf484
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-22 13:04:32 +0800

    Create Relics.php

---
+++ PCPCore-masterBETA/src/PCPCore/Relics.php
@@ -0,0 +1,33 @@
+namespace PCPCore;
+
+use PCPCore\Core;
+
+use pocketmine\Player;
+use pocketmine\Server;
+
+use pocketmine\inventory\Inventory;
+use pocketmine\item\Item;
+
+class Relics {
+  public $main;
+  
+  construct __parent(Core $main)
+  {
+      $this->main = $main;
+  }
+  
+  public function foundRelic() : void
+  {
+    
+  }
+  
+  private isLucky() : bool
+  {
+  
+  }
+  
+  private randomRelic() : string
+  {
+   
+  }
+}
