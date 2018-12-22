8c9b28aff549301e40549d95933f581074fce918
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-22 14:31:56 +0800

    Update Relics.php
    friggin feck i have no life

--- PCPCore-masterBETA/src/PCPCore/Relics.php
+++ PCPCore-masterBETA/src/PCPCore/Relics.php
@@ -6,9 +6,10 @@ use PCPCore\Core;
 
 use pocketmine\Player;
 use pocketmine\Server;
-
-use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\inventory\Inventory;
+use pocketmine\utils\TextFormat as TF;
 
 class Relics {
 	public $main;
@@ -18,23 +19,39 @@ class Relics {
 		$this->main = $main;
 	}
   
-  	public function foundRelic(Player $player) : void
+  	public function foundRelic(Player $player) : bool
   	{
     		if($this->isLucky())
     		{
+			$relic = Item::get(146, 69, 1);
       			switch( strtolower($this->getRandomRelic()) )
       			{
-				case "common": break;
-				case "rare": break;
-				case "extreme": break;
-				case "legendary": break;
-				case "mythic": break;
+				case "common": $relic->setLore([ TF::AQUA. "Common" ]); break;
+				case "rare": $relic->setLore([ TF::YELLOW. "Rare" ]); break;
+				case "exotic": $relic->setLore([ TF::PURPLE. "Exotic" ]); break;
+				case "legendary": $relic->setLore([ TF::DARK_PURPLE. "Legendary" ]); break;
+				case "mythic": $relic->setLore([ TF::BLACK. "Mythic" ]); break;
+				default:
+					return false;
       			}
+			
+			if($player->getInventory()->canAddItem($relic))
+			{
+				$player->getInventory()->addItem($relic);
+			} else {
+				$player->getLevel()->dropItem(new Vector3($player->getX(), $player->getY(), $player->getZ()), $relic));
+			}
+			
+			return true;
     		}
+		return false;
   	}
   
-	public function openRelic(Player $player, string $rarity) : void
+	public function openRelic(Player $player, Item $item) : void
 	{
+		$itemLore = $item->getlore();
+      	 	$itemLevel = (string) TF::clean( $itemLore[0] );
+		//soon..
 	}
   
 	private isLucky() : bool
@@ -46,9 +63,10 @@ class Relics {
 	{
 		$relics = (array) $this->main->premyo->getNested("relics");
 		$sum = (int) array_sum($relics);
+		
     		if($sum > 100)
     		{
-			$this->main->getLogger()->error("§cRELICS percentage sum is more than 100");
+			$this->main->getLogger()->error("§cthe sum of [relic chance rate] is more than 100");
 			return "";
 		}
     
