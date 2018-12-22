71d177def39f7b2638bf760518474503eebbcc5e
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-22 11:50:26 +0800

    Update EventListener.php
    fvck

--- PCPCore-masterBETA/src/PCPCore/EventListener.php
+++ PCPCore-masterBETA/src/PCPCore/EventListener.php
@@ -113,7 +113,7 @@ class EventListener implements Listener{
 				*/
 			}
 			
-			if(array_key_exists((string) $blockid. ".". $blockmeta, $this->plugin->premyo->getNested("breakmoney")))
+			if(array_key_exists($blockid. "-". $blockmeta, $this->plugin->premyo->getNested("breakmoney")))
 			{
 				$pr = explode( "-", $this->plugin->premyo->getNested("breakmoney." . $blockid) );
 				Server::getInstance()->getPluginManager()->getPlugin("EconomyAPI")->addMoney($event->getPlayer(), mt_random($pr[0], $pr[1]));
