55e3d7e80f643db54cb7cfe03d302a8762ea7fd8
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-22 13:36:16 +0800

    Update Relics.php
    proper indent for initial commit

--- PCPCore-masterBETA/src/PCPCore/Relics.php
+++ PCPCore-masterBETA/src/PCPCore/Relics.php
@@ -11,55 +11,55 @@ use pocketmine\inventory\Inventory;
 use pocketmine\item\Item;
 
 class Relics {
-  public $main;
+	public $main;
   
-  construct __parent(Core $main)
-  {
-      $this->main = $main;
-  }
+	construct __parent(Core $main)
+	{
+		$this->main = $main;
+	}
   
-  public function foundRelic(Player $player) : void
-  {
-    if($this->isLucky())
-    {
-      switch( strtolower($this->getRandomRelic()) )
-      {
-        case "common": break;
-        case "rare": break;
-        case "extreme": break;
-        case "legendary": break;
-        case "mythic": break;
-      }
-    }
-  }
+  	public function foundRelic(Player $player) : void
+  	{
+    		if($this->isLucky())
+    		{
+      			switch( strtolower($this->getRandomRelic()) )
+      			{
+				case "common": break;
+				case "rare": break;
+				case "extreme": break;
+				case "legendary": break;
+				case "mythic": break;
+      			}
+    		}
+  	}
   
-  public function openRelic(Player $player, string $rarity) : void
-  {
-  }
+	public function openRelic(Player $player, string $rarity) : void
+	{
+	}
   
-  private isLucky() : bool
-  {
-    return (mt_rand(1, 10) <= 2);
-  }
+	private isLucky() : bool
+	{
+		return (mt_rand(1, 10) <= 2);
+	}
   
-  private getRandomRelic() : string
-  {
-    $relics = (array) $this->main->premyo->getNested("relics");
-    $sum = (int) array_sum($relics);
-    if($sum > 100)
-    {
-      $this->main->getLogger()->error("§cRELICS percentage sum is more than 100");
-      return "";
-    }
+	private getRandomRelic() : string
+	{
+		$relics = (array) $this->main->premyo->getNested("relics");
+		$sum = (int) array_sum($relics);
+    		if($sum > 100)
+    		{
+			$this->main->getLogger()->error("§cRELICS percentage sum is more than 100");
+			return "";
+		}
     
-    $rand = mt_rand(1, $sum);
-    foreach ($relics as $relic => $chance)
-	  {
-      $rand -= $chance;
-      if ($rand <= 0)
-	    {
-		    return $relic;
-      }
-    }
-  }
+		$rand = mt_rand(1, $sum);
+		foreach ($relics as $relic => $chance)
+		{
+     			$rand -= $chance;
+      			if ($rand <= 0)
+	    		{
+		    		return $relic;
+      			}
+    		}
+  	}
 }
