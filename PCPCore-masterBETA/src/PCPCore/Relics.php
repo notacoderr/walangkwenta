c96445fcde76a55e9fad8fe3d3e95ae631f8cef3
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-22 13:32:56 +0800

    Update Relics.php
    initial commit for relics

--- PCPCore-masterBETA/src/PCPCore/Relics.php
+++ PCPCore-masterBETA/src/PCPCore/Relics.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace PCPCore;
 
 use PCPCore\Core;
@@ -16,18 +18,48 @@ class Relics {
       $this->main = $main;
   }
   
-  public function foundRelic() : void
+  public function foundRelic(Player $player) : void
   {
-    
+    if($this->isLucky())
+    {
+      switch( strtolower($this->getRandomRelic()) )
+      {
+        case "common": break;
+        case "rare": break;
+        case "extreme": break;
+        case "legendary": break;
+        case "mythic": break;
+      }
+    }
+  }
+  
+  public function openRelic(Player $player, string $rarity) : void
+  {
   }
   
   private isLucky() : bool
   {
-  
+    return (mt_rand(1, 10) <= 2);
   }
   
-  private randomRelic() : string
+  private getRandomRelic() : string
   {
-   
+    $relics = (array) $this->main->premyo->getNested("relics");
+    $sum = (int) array_sum($relics);
+    if($sum > 100)
+    {
+      $this->main->getLogger()->error("§cRELICS percentage sum is more than 100");
+      return "";
+    }
+    
+    $rand = mt_rand(1, $sum);
+    foreach ($relics as $relic => $chance)
+	  {
+      $rand -= $chance;
+      if ($rand <= 0)
+	    {
+		    return $relic;
+      }
+    }
   }
 }
