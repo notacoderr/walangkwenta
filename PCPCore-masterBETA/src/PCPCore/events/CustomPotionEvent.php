90488b7d5ddad7c6b6819d6061811b752ef355dc
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-26 22:10:22 +0800

    Update CustomPotionEvent.php

--- PCPCore-masterBETA/src/PCPCore/events/CustomPotionEvent.php
+++ PCPCore-masterBETA/src/PCPCore/events/CustomPotionEvent.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace PCPCore\events;
+namespace CorePCP\events;
 
-use PCPCore\Core;
+use CorePCP\Core;
 
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
