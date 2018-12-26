8aa73151fa8ced040fe0f17931f4dcc5687c5b72
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-12-26 21:40:47 +0800

    Update and rename FlyCommand.php to GlideCommand.php

--- PCPCore-masterBETA/src/PCPCore/cmds/GlideCommand.php
+++ PCPCore-masterBETA/src/PCPCore/cmds/GlideCommand.php
@@ -10,28 +10,29 @@ use pocketmine\command\{
 };
 use pocketmine\Player;
 
-class FlyCommand extends PluginCommand{
+class GlideCommand extends PluginCommand{
 
     /** @var Core */
     private $plugin;
 
     public function __construct($name, Core $plugin){
         parent::__construct($name, $plugin);
-        $this->setDescription("Enable OR Disable /fly mode!");
+        $this->setDescription("Enable or disable /glide mode!");
         $this->plugin = $plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args) : bool{
         if(!$sender instanceof Player) return false;
-        if($sender->hasPermission("pcp.fly")){
+        if($sender->hasPermission("pcp.glide")){
             if(!$sender->getAllowFlight()){
                 $sender->setAllowFlight(true);
-                $sender->sendMessage("§8§l(§b!§8)§r §7Your ability to fly has been §l§aENABLED§r§7!");
-                //$sender->addTitle("§l","a");
+                $sender->sendMessage("§8§l(§b!§8)§r §7Your ability to glide has been §l§aENABLED§r§7!");
+                $sender->addTitle("§l§bGlide","§aEnabled");
             }else{
                 $sender->setAllowFlight(false);
                 $sender->setFlying(false);
-                $sender->sendMessage("§8§l(§b!§8)§r §7Your ability to fly has been §l§cDISABLED§r§7!");
+                $sender->sendMessage("§8§l(§b!§8)§r §7Your ability to glide has been §l§cDISABLED§r§7!");
+                $sender->addTitle("§l§bGlide", "§cDisabled");
             }
         }else{
             $sender->sendMessage(Core::PERM_RANK);
